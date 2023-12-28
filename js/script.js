b0d5c088deb24fa387743562d01e1508b25c6048
Md. Al-Amin <145653719+mdalamin006@users.noreply.github.com>
2023-12-28 18:33:05 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// JavaScript code 
+
+const input = document.querySelector("#phone");
+window.intlTelInput(input, {
+    utilsScript: "https://cdn.jsdelivr.net/npm/intl-tel-input@18.2.1/build/js/utils.js",
+});
+
+
+// jQurey Code
+$(document).ready(function () {
+
+
+
+});
